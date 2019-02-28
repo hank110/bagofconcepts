5c5f5eec03cf9b5d115d19a5938c5f64e3229649
hank110 <hank1111@gmail.com>
2019-02-28 19:49:50 +0900

    setup.py initial update

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
 from distutils.core import setup
 setup(
-  name = 'YOURPACKAGENAME',         # How you named your package folder (MyLib)
-  packages = ['YOURPACKAGENAME'],   # Chose the same as "name"
+  name = 'boc',         # How you named your package folder (MyLib)
+  packages = ['boc'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
-  author = 'YOUR NAME',                   # Type in your name
-  author_email = 'your.email@domain.com',      # Type in your E-Mail
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
+  author = 'Han Kyul Kim',                   # Type in your name
+  author_email = 'hank1111@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/hank110/bag-of-concepts',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
+  keywords = ['bag of concepts', 'word2vec clustering', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+          'scipy',
+          'scikit-learn',
+          'gensim',
+          'spherecluster'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
