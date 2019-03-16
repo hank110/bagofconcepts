c8238784c6b369fb5bc680126b8cf16e01699b13
hank110 <hank1111@gmail.com>
2019-03-16 21:14:46 +0900

    Training & Saving tests passed

--- boc/boc.py
+++ boc/boc.py
@@ -29,21 +29,21 @@ class BOCModel():
         self.iterations=iterations
 
     
-    def fit(self, w2v_saver=0, output_path=""):
+    def fit(self, save_path=""):
         
         if self.model_path is not None:
             wv, idx2word=load_w2v(self.doc_path)
         else:
             wv, idx2word=train_w2v(self.doc_path, self.embedding_dim, 
-                self.context, self.min_freq, self.iterations, w2v_saver)
+                self.context, self.min_freq, self.iterations, save_path)
 
         wv_cluster_id=_cluster_wv(wv, self.num_concept)
         bow=_create_bow(idx2word, self.doc_path)
         w2c=_create_w2c(idx2word, wv_cluster_id, self.num_concept)
         boc=_apply_cfidf(safe_sparse_dot(bow, w2c))
         
-        if output_path:
-           _save_boc(output_path, boc, idx2word, wv_cluster_id)
+        if save_path:
+           _save_boc(save_path, boc, idx2word, wv_cluster_id)
             
         return boc, [wc_pair for wc_pair in zip(idx2word, wv_cluster_id)], idx2word
 
@@ -103,15 +103,15 @@ def tokenize(doc_path):
             yield doc.rstrip().split(" ")
 
 
-def train_w2v(doc_path, embedding_dim, context, min_freq, iterations, save=0):
+def train_w2v(doc_path, embedding_dim, context, min_freq, iterations, save_path=""):
     tokenized_docs=tokenize(doc_path)
     model=Word2Vec(size=embedding_dim, window=context, min_count=min_freq, sg=1)
     model.build_vocab(tokenized_docs)
     model.train(tokenized_docs, total_examples=model.corpus_count, epochs=iterations)
     
-    if (save==1):
-        model_name="w2v_model_d%d_w%d" %(embedding_dim, context) 
-        model.wv.save_word2vec_format(model_name)
+    if save_path:
+        model_name="/w2v_model_d%d_w%d" %(embedding_dim, context) 
+        model.wv.save_word2vec_format(save_path+model_name)
 
     return model.wv.vectors, model.wv.index2word
 
--- test/test_0.py
+++ test/test_0.py
@@ -1,7 +1,7 @@
 import boc
 
 a=boc.BOCModel(doc_path="../sample/sample_data/sample_articles.txt")
-mtx,w2c,idx=a.fit("./")
+mtx,w2c,idx=a.fit(save_path="./")
 
 print(mtx.shape)
 print(w2c[203])
