6a1eb9024ba9ce8808644e6040d64ddbdc5c0b21
hank110 <hank1111@gmail.com>
2019-03-18 22:04:45 +0900

    setup.py github url path revised

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   description = 'This is python implementation of Bag-of-Concepts, as proposed by the paper "Bag-of-Concepts: Comprehending Document Representation through Clustering Words in Distributed Representation"',  
   author = 'Hank Kim',                   
   author_email = 'hank1111@gmail.com',
-  url = 'https://github.com/hank110/boc',
+  url = 'https://github.com/hank110/bagofconcepts',
   download_url = 'https://github.com/hank110/boc/archive/v0.0.1.tar.gz',
   keywords = ['bag of concepts', 'boc', 'word2vec clustering', 'text mining', 'NLP', 'machine learning'],
   install_requires=[       
